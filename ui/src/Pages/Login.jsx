0c10cef0783b9646f87964b4c262ec0035b23f02
Kashish <kashish042001@gmail.com>
2023-05-28 21:19:50 +0530

    Login Page

--- ui/src/Pages/Login.jsx
+++ ui/src/Pages/Login.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import Register from './Register'
 
 const Login = () => {
   return (
-    <div className='mt-4 grow flex flex-col justify-center mb-64'>
+    <div className='mt-4 grow flex flex-col justify-center mb-32'>
       <h1 className='text-4xl text-center mb-4'>Login</h1>
       <form action="" className='max-w-md mx-auto' >
         <input type="email" placeholder='your@email.com' />
