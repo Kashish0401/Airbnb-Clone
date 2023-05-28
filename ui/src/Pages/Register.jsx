e0d99bf85349a764cf8089e4a6b6c5220b54c492
Kashish <kashish042001@gmail.com>
2023-05-29 01:22:08 +0530

    Register Page

--- ui/src/Pages/Register.jsx
+++ ui/src/Pages/Register.jsx
@@ -1,17 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
+import axios from 'axios';
 
 const Register = () => {
+
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const registerUser = async(ev) => {
+    ev.preventDefault();
+    try {
+      await axios.post('/register', {
+        name,
+        email,
+        password,
+      });
+      alert('Registration completed!');
+    }
+    catch (e) {
+      alert('Registration failed!');
+    }
+  }
+
   return (
     <div className='mt-4 grow flex flex-col justify-center mb-32'>
-          <h1 className='text-4xl text-center mb-4'>Registeration Page</h1>
-          <form action="" className='max-w-md mx-auto'>
-              <input type="text" placeholder='Your Name' />
-              <input type="email" placeholder='your@email.com' />
-              <input type="password" placeholder='password' />
-              <button className='primary'>Register</button>
-          </form>
-          <div className='max-w-md mx-auto text-gray-500 mt-2'>Already have an account? <Link to={"/login"} className='underline text-black text-center'>Login</Link></div>
+      <h1 className='text-4xl text-center mb-4'>Registeration Page</h1>
+      <form onSubmit={registerUser} className='max-w-md mx-auto'>
+        <input type="text"
+          placeholder='Your Name'
+          value={name}
+          onChange={ev => setName(ev.target.value)} />
+        <input type="email"
+          placeholder='your@email.com'
+          value={email}
+          onChange={ev => setEmail(ev.target.value)} />
+        <input type="password"
+          placeholder='password'
+          value={password}
+          onChange={ev => { setPassword(ev.target.value) }}
+        />
+        <button className='primary'>Register</button>
+      </form>
+      <div className='max-w-md mx-auto text-gray-500 mt-2'>Already have an account? <Link to={"/login"} className='underline text-black text-center'>Login</Link></div>
     </div>
   )
 }
