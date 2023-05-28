ae6ea510baa5b82e2eab48f49fa1c59d7d925d22
Kashish <kashish042001@gmail.com>
2023-05-28 18:14:09 +0530

    LoginPage

--- ui/src/Pages/Login.jsx
+++ ui/src/Pages/Login.jsx
@@ -2,9 +2,14 @@ import React from 'react'
 
 const Login = () => {
   return (
-    <>
-      login
-    </>
+    <div className='mt-4 grow flex flex-col justify-center mb-64'>
+      <h1 className='text-4xl text-center mb-4'>Login</h1>
+      <form action="" className='max-w-md mx-auto' >
+        <input type="email" placeholder='your@email.com' />
+        <input type="password" placeholder='password' />
+        <button className='primary'>Login</button>
+      </form>
+    </div>
   )
 }
 
