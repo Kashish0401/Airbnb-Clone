65a23e398f268021d2ff1965636705e0620afe50
Kashish <kashish042001@gmail.com>
2023-06-09 20:42:16 +0530

    upload from device

--- ui/src/Pages/PlacesPage.jsx
+++ ui/src/Pages/PlacesPage.jsx
@@ -44,6 +44,22 @@ const Places = () => {
     );
   }
 
+  async function uploadPhoto(ev) {
+    //console.log(ev);
+    const files = ev.target.files;
+    //console.log({ files });
+    const data = new FormData();
+    for (let i = 0; i < files.length; i++){
+      data.append('photos', files[i]);
+    }
+    const {data:filename} = await axios.post('/uploads', data, {
+      headers: { 'Content-type': 'multipart/form-data' }
+    });
+    setAddedPhotos(prev => {
+      return [...prev, filename];
+    })
+  }
+
   return (
     <>
       {action !== 'new' && (
@@ -77,19 +93,20 @@ const Places = () => {
             <button onClick={addPhotoByLink}
               className="bg-gray-200 px-4 rounded-2xl">Add&nbsp;photo</button>
           </div>
-          <div>
+          <div className="mt-2 grid gap-2 grid-cols-3 md:grid-cols-4 lg:grid-cols-6">
             {addedPhotos.length > 0 && addedPhotos.map(link => (
               <div>
-                <img src={'http://localhost:4000/uploads/' + link} alt="" />
+                <img src={'http://localhost:4000/uploads/' + link} className="rounded-2xl" />
               </div>
             ))
             }
-            <button className="p-4 border rounded-xl flex flex-col items-center text-gray-600">
+            <label className="p-4 border rounded-xl flex flex-col items-center justify-center  text-gray-600 cursor-pointer">
+              <input type="file" multiple className="hidden" onChange={uploadPhoto}/>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M12 16.5V9.75m0 0l3 3m-3-3l-3 3M6.75 19.5a4.5 4.5 0 01-1.41-8.775 5.25 5.25 0 0110.233-2.33 3 3 0 013.758 3.848A3.752 3.752 0 0118 19.5H6.75z" />
               </svg>
               Upload from your device
-            </button>
+            </label>
           </div>
           {preInput('Description', 'A small description about your place')}
           <textarea value={description} onChange={ev => setDescription(ev.target.value)} />
