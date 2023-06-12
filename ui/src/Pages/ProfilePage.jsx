cfcb86c8ee45910c4ccdba2a1e23198096e28a59
Kashish <kashish042001@gmail.com>
2023-06-12 22:14:17 +0530

    styling

--- ui/src/Pages/ProfilePage.jsx
+++ ui/src/Pages/ProfilePage.jsx
@@ -18,9 +18,9 @@ const Profile = () => {
     }
 
   return (
-    <div className="text-center mt-8 max-w-lg mx-auto">
+    <div className="text-center max-w-lg mx-auto">
           Logged in as {user?.name} ({user?.email}) <br />
-          <button onClick={logout} className="primary max-w-sm mt-2">Logout</button>
+          <button onClick={logout} className="primaryColor py-2 rounded-2xl px-6 mt-2">Logout</button>
           
     </div>
   )
