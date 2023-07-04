b4cf41a2723e78affe62f2a8f3ee639c1a540a6c
Kashish <kashish042001@gmail.com>
2023-07-04 23:27:58 +0530

    place

--- ui/src/Pages/PlacePage.jsx
+++ ui/src/Pages/PlacePage.jsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 const PlacePage = () => {
+  const { id } = useParams();
+  const [place, setPlace] = useState(null);
+  useEffect(() => {
+    if (!id) {
+      return;
+    }
+    axios.get(`/places/${id}`).then(response => {
+      setPlace(response.data);
+    });
+  }, [id]);
+
   return (
     <>
       Place Page
